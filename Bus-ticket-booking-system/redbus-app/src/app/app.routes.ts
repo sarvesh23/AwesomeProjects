99dd3969fa8fb110c8d5fc3256f71a13a03ebaab
Sarvesh23 <shreyas2335@gmail.com>
2025-01-08 00:02:11 +0530

    Login signup and home page initial setup

--- Bus-ticket-booking-system/redbus-app/src/app/app.routes.ts
+++ Bus-ticket-booking-system/redbus-app/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './component/login-page/login-page.component';
+import { SignUpPageComponent } from    './component/sign-up-page/sign-up-page.component';
+import { HomeComponent } from './component/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", redirectTo:"login", pathMatch:"full"},
+    {path:"login", component:LoginPageComponent},
+    {path:"signUp", component:SignUpPageComponent},
+    {path:"home", component:HomeComponent},
+  ];
